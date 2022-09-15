6f776657e66b0c9986731bde275c9b28627aafb9
Wagner Rigoli da Rosa <wagnerr2@gmail.com>
2022-09-14 21:57:37 -0300

    Before publication.

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,58 @@
+<template>
+  <header class="has-text-centered">
+    <h1>
+      <img src="../assets/logo.jpg" alt="WR2 Tracker" class="logo" />
+    </h1>
+    <button class="button is-dark" @click="alterarTema">
+      {{textoBotao}}
+    </button>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BarraLateral",
+  emits: ['aoTemaAlterado'],
+  data() {
+    return {
+      modoEscuroAtivo: false
+    }
+  },
+  computed: {
+    textoBotao () {
+      if (this.modoEscuroAtivo) {
+        return "Desativar modo escuro"
+      }
+      return "Ativar modo escuro"
+    }
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo
+      this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+    },
+  },
+})
+</script>
+
+<style scoped>
+header {
+  background: #0d3d65;
+  width: 100%;
+  height: 100vh;
+  padding: 1rem;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+.logo {
+  width: 180px;
+  height: auto;
+  border-radius: 50%;
+}
+</style>
---
+++ src/components/Box.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'MyBox'
+})
+</script>
+
+<style scoped>
+.box {
+  background: #b8c1c1;
+}
+</style>
