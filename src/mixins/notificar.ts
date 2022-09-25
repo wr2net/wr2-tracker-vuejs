016b43a61771197538666c5e750ae07e40af1fea
Wagner Rigoli da Rosa <wagnerr2@gmail.com>
2022-09-25 17:57:48 -0300

    Finalizando implementação do projeto WR2 Track referente ao curso Vue3 Avançado.

---
+++ src/mixins/notificar.ts
@@ -0,0 +1,9 @@
+import {TipoNotificacao} from "@/interfaces/INotificacao";
+import {NOTIFICAR} from "@/store/tipo-mutacoes";
+import {store} from '@/store';
+
+export const notificarMixin = {
+  methods: {
+
+  }
+}
--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -35,8 +35,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import {useStore} from "@/store";
-import {ADICIONA_PROJETO, ALTERA_PROJETO, NOTIFICAR} from "@/store/tipo-mutacoes";
+import {ADICIONA_PROJETO, ALTERA_PROJETO} from "@/store/tipo-mutacoes";
 import {TipoNotificacao} from "@/interfaces/INotificacao";
+import useNotificador from '@/hooks/notificador';
 
 export default defineComponent({
   name: "MyProjetos",
@@ -67,18 +68,16 @@ export default defineComponent({
         this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
       this.nomeDoProjeto = "";
-      this.store.commit(NOTIFICAR, {
-        titulo: 'Novo projeto salvo',
-        texto: "Prontinho ;) seu projeto já está disponível.",
-        tipo: TipoNotificacao.SUCCESS
-      })
+      this.notificar(TipoNotificacao.SUCCESS, 'Novo projeto salvo', "Prontinho ;) seu projeto já está disponível.");
       this.$router.push('/projetos')
-    }
+    },
   },
   setup () {
     const store = useStore()
+    const {notificar} = useNotificador()
     return {
       store,
+      notificar
     }
   }
 });
